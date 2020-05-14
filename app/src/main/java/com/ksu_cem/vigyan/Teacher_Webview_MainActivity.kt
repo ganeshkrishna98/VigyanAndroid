2863ccee27d04ac7d04bbd05f561935fb4e2977a
Ganesh Krishna <ganeshkrishna98@gmail.com>
2020-05-15 03:38:14 +0530

    added progress bar

--- app/src/main/java/com/ksu_cem/vigyan/Teacher_Webview_MainActivity.kt
+++ app/src/main/java/com/ksu_cem/vigyan/Teacher_Webview_MainActivity.kt
@@ -1,9 +1,12 @@
 package com.ksu_cem.vigyan
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
 
 class Teacher_Webview_MainActivity : AppCompatActivity() {
 
@@ -13,14 +16,30 @@ class Teacher_Webview_MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         val webView = findViewById<WebView>(R.id.tlclnt)
-        val setting = webView.settings;
+        val setting = webView.settings
         setting.javaScriptEnabled = true
+
+        val ProgressBar = findViewById<ProgressBar>(R.id.progressBar)
+
         webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
                 view?.loadUrl(url)
                 return true
             }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                ProgressBar.isShown
+                super.onPageStarted(view, url, favicon)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                ProgressBar.setVisibility(View.INVISIBLE)
+                super.onPageFinished(view, url)
+            }
+
         }
         webView.loadUrl("http://vigyan-portal.herokuapp.com/")
     }
 }
+
+
--- app/src/main/java/com/ksu_cem/vigyan/ui/notification_act/Comp_not_MainActivity.kt
+++ app/src/main/java/com/ksu_cem/vigyan/ui/notification_act/Comp_not_MainActivity.kt
@@ -1,9 +1,12 @@
 package com.ksu_cem.vigyan.ui.notification_act
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import com.ksu_cem.vigyan.R
 
 class Comp_not_MainActivity : AppCompatActivity() {
@@ -14,12 +17,24 @@ class Comp_not_MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         val webView = findViewById<WebView>(R.id.csnotifwv)
-        val setting = webView.settings;
+        val setting = webView.settings
         setting.javaScriptEnabled = true
+        val ProgressBar = findViewById<ProgressBar>(R.id.progressBar)
+
         webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
                 view?.loadUrl(url)
                 return true
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                ProgressBar.isShown
+                super.onPageStarted(view, url, favicon)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                ProgressBar.setVisibility(View.INVISIBLE)
+                super.onPageFinished(view, url)
             }
         }
         webView.loadUrl("http://vigyan-portal.herokuapp.com/")
